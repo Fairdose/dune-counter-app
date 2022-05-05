495f6bfb388968065397c9e948b725723003319d
Batur <fairdose@cyberservices.com>
2022-05-05 11:06:43 +0300

    Room indent & css improvements - Joined room shown - Removed Link from home screen

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script>
     import {Router, Route, navigate} from 'svelte-navigator'
-    import Home from "./routes/Home.svelte"
+    import Home from './routes/Home.svelte'
     import Counter from './routes/[room].svelte'
-
 </script>
 
 <Router>
